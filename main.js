0c9502b62f7abf9ad016f89f4249f363ac66c576
Gabriel Prisco_N <gabrielmarcosprisco@gmail.com>
2024-03-09 11:10:06 -0300

    Nova funcionalidade, tranformar texto em audio

--- main.js
+++ main.js
@@ -1,72 +1,26 @@
-const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const fs = require('fs');
-const speech = require('@google-cloud/speech');
-const client2 = new speech.SpeechClient();
+import codeqr from 'qrcode-terminal';
+import pkg from 'whatsapp-web.js';
+const { generate } = codeqr;
+const { Client, LocalAuth, MessageMedia } = pkg;
+
+
+
+import Funcoes from './scripts.js';
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = 'composed-region-416600-af6bc7913fdc.json'; // Set the path to your Google Cloud service account key.
 
-
+const funcoes = Funcoes
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: 'yourFolderName'
-    })
+  authStrategy: new LocalAuth({
+      dataPath: 'yourFolderName'
+  })
 });
- 
-class Funcoes {
-    msghitter(msgR, msgH){
-        client.on('message', async (message) => {
-            if (message.body === `!${msgR}`) {
-                await client.sendMessage(message.from, msgH);
-            }
-        });
-        
-    }
- 
-}
-
-async function trans(audioName) {
-    // Creates a client
-  const client2 = new speech.SpeechClient();
-  // The path to the remote LINEAR16 file
-  const filename = audioName;
-  const file = fs.readFileSync(filename)
-  const audioBytes = file.toString('base64')
-
-  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-  const audio = {
-    content: audioBytes
-  };
-  const config = {
-    encoding: 'MP3',
-    sampleRateHertz: 16000,
-    languageCode: 'pt-BR',
-  };
-  const request = {
-    audio: audio,
-    config: config,
-  };
-
-  // Detects speech in the audio file
-  const [response] = await client2.recognize(request);
-  const transcription = response.results
-    .map(result => result.alternatives[0].transcript)
-    .join('\n');
-  console.log(`Transcription: ${transcription}`);
-  client.on('message', async (message) => {
-        await client.sendMessage(message.from, transcription);
-});
-}
-
-
-const funcoes = new Funcoes()
-
 
 function main () {
 
     client.on('qr', (qr) => {
-        qrcode.generate(qr, { small: true });
+        generate(qr, { small: true });
     });
 
     client.on('ready', () => {
@@ -78,36 +32,30 @@ function main () {
     });
 
     client.on('message', async (message) => {
-        if (message.hasMedia) {
+        if (message.type == 'ptt' || message.type == 'audio') {
           const media = await message.downloadMedia()
-  
-          const audioBytes = media.data
-
-          // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-          const audio = {
-            content: audioBytes
-          };
-          const config = {
-            encoding: 'OGG_OPUS',
-            sampleRateHertz: 16000,
-            languageCode: 'pt-BR',
-          };
-          const request = {
-            audio: audio,
-            config: config,
-          };
-
-          // Detects speech in the audio file
-          const [response] = await client2.recognize(request);
-          const transcription = response.results
-            .map(result => result.alternatives[0].transcript)
-            .join('\n');
-          console.log(`Transcription: ${transcription}`);
-        
-          await message.reply(transcription)
-        
-            
+          const data = await funcoes.trans(media)
+          await message.reply(data)
+         
          }
+     });
+
+     client.on('message', async (message) => {
+        if (message.body.startsWith('!audio ')){
+            const frase = message.body.replace('!audio ', '')
+
+            try {    
+                const audioPath = await funcoes.viraAudio(frase, message) // Caminho do arquivo de �udio
+                
+
+                const chat = await message.getChat();
+                const audio = MessageMedia.fromFilePath(audioPath);
+                chat.sendMessage(audio, { sendAudioAsVoice: true })
+                
+            } catch (error) {
+                console.error('Erro ao gerar ou enviar o áudio:', error);
+            }
+        }
      });
 
     funcoes.msghitter('ping','pong')

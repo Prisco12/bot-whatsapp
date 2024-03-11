f756c7bb6b6f5f6917960ef862adb4317cc82d23
Gabriel Prisco <gabrielmarcosprisco@gmail.com>
2024-03-11 16:27:14 -0300

    tratamento de mensagens de audio e stickers

--- main.js
+++ main.js
@@ -1,123 +1,145 @@
-import qrcode from 'qrcode-terminal';
-import pkg from 'whatsapp-web.js';
-const { Client, LocalAuth, MessageMedia, Events, Globals} = pkg;
+import qrcode from "qrcode-terminal";
+import pkg from "whatsapp-web.js";
+const { Client, LocalAuth, MessageMedia, Events, Globals } = pkg;
 
+import Funcoes from "./scripts.js";
+process.env.GOOGLE_APPLICATION_CREDENTIALS =
+  "composed-region-416600-af6bc7913fdc.json"; // Set the path to your Google Cloud service account key.
 
-import Funcoes from './scripts.js';
-process.env.GOOGLE_APPLICATION_CREDENTIALS = 'composed-region-416600-af6bc7913fdc.json'; // Set the path to your Google Cloud service account key.
-
-
-const funcoes = Funcoes
+const funcoes = Funcoes;
 
 const client = new Client({
   authStrategy: new LocalAuth({
-      dataPath: 'yourFolderName'
+    dataPath: "yourFolderName",
   }),
   puppeteer: {
-		args: ['--no-sandbox'],
-	}
+    args: ["--no-sandbox"],
+  },
 });
 
-function main () {
-    
-    client.on('qr', (qr) => {
-        qrcode.generate(qr, { small: true });
-    });
+function main() {
+  client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
 
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
+  client.on("ready", () => {
+    console.log("Client is ready!");
+  });
 
-    client.on('message', (message) => {
-        console.log(message.body);
-    });
+  client.on("message", (message) => {
+    console.log(message.body);
+  });
 
-    client.on('message', async (message) => {
-        if (message.type == 'ptt' || message.type == 'audio') {
-          const media = await message.downloadMedia()
-          const data = await funcoes.trans(media)
-          await message.reply(data)
-         
-         }
-     });
+  client.on("message", async (message) => {
+    if (message.type == "ptt" || message.type == "audio") {
+      const media = await message.downloadMedia();
+      const data = await funcoes.trans(media);
+      await message.reply(data);
+    }
+  });
 
-    client.on('message', async (message) => {
-        if (message.body.startsWith('!audio ')){
-            const frase = message.body.replace('!audio ', '')
-            try {    
-                const audioPath = await funcoes.viraAudio(frase, message) // Caminho do arquivo de �udio
-                const chat = await message.getChat();
-                const audio = MessageMedia.fromFilePath(audioPath);
-                await chat.sendMessage(audio, { sendAudioAsVoice: true })
-             } catch (error) {
-                 console.error('Erro ao gerar ou enviar o áudio:', error);
-             }
-        }
-    });
+  client.on("message", async (message) => {
+    if (message.body.startsWith("!audio ")) {
+      const frase = message.body.replace("!audio ", "");
+      try {
+        const audioPath = await funcoes.viraAudio(frase, message); // Caminho do arquivo de �udio
+        const chat = await message.getChat();
+        const audio = MessageMedia.fromFilePath(audioPath);
+        await chat.sendMessage(audio, { sendAudioAsVoice: true });
+      } catch (error) {
+        console.error("Erro ao gerar ou enviar o áudio:", error);
+      }
+    }
+  });
 
+  client.on("message", async (message) => {
+    if (message.type == "image") {
+      const imagem = await message.downloadMedia();
+      if (message.body.startsWith("!sticker")) {
+        const stickerMedia = new MessageMedia(
+          "image/jpeg",
+          imagem.data,
+          "image.jpeg"
+        );
+        client.sendMessage(message.from, stickerMedia, {
+          sendMediaAsSticker: true,
+        });
+      }
+    }
+  });
 
-    client.on('message', async (message) => {
-        if(message.type == 'image'){
-        const imagem = await message.downloadMedia()
-        if (message.body.startsWith('!sticker')) {
-            const stickerMedia = new MessageMedia('image/jpeg', imagem.data, 'image.jpeg' );
-            client.sendMessage(message.from, stickerMedia, {sendMediaAsSticker: true});
-        }
-        }
-    })
+  const sentMessages = new Map();
 
-    const sentMessages = new Map();
+  client.on("message", async (msg) => {
+    console.log(msg);
+    if (msg.type == "chat") {
+      sentMessages.set(msg.timestamp, { body: msg.body, type: msg.type });
+    }
+    if (msg.type == "image" && msg._data.isViewOnce == false) {
+      const mage = await msg.downloadMedia();
+      sentMessages.set(msg.timestamp, {
+        body: mage.data,
+        type: msg.type,
+        legenda: msg.body,
+      });
+    }
+    if (msg.type == "sticker") {
+      const mage = await msg.downloadMedia();
+      sentMessages.set(msg.timestamp, {
+        body: mage.data,
+        type: msg.type,
+      });
+    }
+    if (msg.type == "ptt" || msg.type == "audio") {
+      const media = await msg.downloadMedia();
+      sentMessages.set(msg.timestamp, { body: media.data, type: msg.type });
+    }
+    if (msg._data.isViewOnce == true && msg.type == "image") {
+      const mage = await msg.downloadMedia();
+      const imageRevoked = new MessageMedia("image/jpeg", mage.data);
+      await client.sendMessage(msg.from, imageRevoked, {
+        caption: `"${msg.body}"\n\nDeus Está vendo 👀`,
+      });
+    }
+    console.log(sentMessages);
+  });
 
-    client.on('message', async (msg) => {
-        console.log(msg)
-        if(msg.type == 'chat'){
-            sentMessages.set(msg.timestamp, {body: msg.body, type: msg.type}, );
-        }
-        if(msg.type == 'image' && msg._data.isViewOnce == false){
-            const mage = await msg.downloadMedia()
-            sentMessages.set(msg.timestamp, {body: mage.data, type: msg.type, legenda: msg.body}, );
-        }
+  client.on("message_revoke_everyone", async (revokedMsg) => {
+    const revokedMsgId = revokedMsg.timestamp;
 
-        if(msg._data.isViewOnce == true && msg.type == 'image'){
-            const mage = await msg.downloadMedia()
-            const imageRevoked = new MessageMedia('image/jpeg', mage.data);
-            await client.sendMessage(msg.from, imageRevoked,{ caption: `"${msg.body}"\n\nDeus Está vendo 👀` });
-        }
-        
+    if (sentMessages.has(revokedMsgId)) {
+      const originalMsg = sentMessages.get(revokedMsgId);
+      console.log(originalMsg);
+      // Reenvia a mensagem revogada
+      if (originalMsg.type == "image") {
+        const imageRevoked = new MessageMedia("image/jpeg", originalMsg.body);
+        await client.sendMessage(revokedMsg.from, imageRevoked, {
+          caption: `"${originalMsg.legenda}"\n\nVer uma vez Jamais - Deus Está vendo 👀`,
+        });
+      }
+      if (originalMsg.type == "sticker") {
+        const imageRevoked = new MessageMedia("image/jpeg", originalMsg.body);
+        await client.sendMessage(revokedMsg.from, imageRevoked, {
+          sendMediaAsSticker: true,
+        });
+      }
+      if (originalMsg.type == "chat") {
+        await revokedMsg.reply(
+          `Deus Está vendo 👀\n\nMensagem Apagada: " ${originalMsg.body} "`
+        );
+      }
+      if (originalMsg.type == "ptt" || originalMsg.type == "audio") {
+        const mediaRkd = new MessageMedia("audio/ogg", originalMsg.body);
+        await client.sendMessage(revokedMsg.from, mediaRkd);
+      }
+    }
+  });
 
-       
+  funcoes.msghitter("ping", "pong");
 
-        console.log(sentMessages)
-         
-    });
-     
-    client.on('message_revoke_everyone', async (revokedMsg) => {
+  funcoes.msghitter("pong", "ping");
 
-        const revokedMsgId = revokedMsg.timestamp;
-        
-        if (sentMessages.has(revokedMsgId)) {
-             const originalMsg = sentMessages.get(revokedMsgId);
-             console.log(originalMsg)
-             // Reenvia a mensagem revogada
-             if(originalMsg.type == 'image'){
-                const imageRevoked = new MessageMedia('image/jpeg', originalMsg.body);
-                await client.sendMessage(revokedMsg.from, imageRevoked,{ caption: `"${originalMsg.legenda}"\n\nDeus Está vendo 👀` });
-             }
-             if(originalMsg.type == 'chat'){
-                await revokedMsg.reply(`Deus Está vendo 👀\n\nMensagem Apagada: " ${originalMsg.body} "`);
-             }
-         }
-    });
-
-    
-
-
-   
-    funcoes.msghitter('ping','pong')
-    
-    funcoes.msghitter('pong','ping')
-
-    client.initialize()
+  client.initialize();
 }
 
-main()
+main();
--- scripts.js
+++ scripts.js
@@ -1,71 +1,66 @@
-import { SpeechClient } from '@google-cloud/speech'
-import pkg from 'whatsapp-web.js';
+import { SpeechClient } from "@google-cloud/speech";
+import pkg from "whatsapp-web.js";
 const { Client, LocalAuth } = pkg;
 
-import gTTS from 'gtts'
+import gTTS from "gtts";
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: 'yourFolderName'
-    })
-  });
+  authStrategy: new LocalAuth({
+    dataPath: "yourFolderName",
+  }),
+});
 
 class Funcoes {
-    msghitter(msgR, msgH){
-        client.on('message', async (message) => {
-            if (message.body === `!${msgR}`) {
-                await client.sendMessage(message.from, msgH);
-            }
-        });
-        
-    }
+  msghitter(msgR, msgH) {
+    client.on("message", async (message) => {
+      if (message.body === `!${msgR}`) {
+        await client.sendMessage(message.from, msgH);
+      }
+    });
+  }
 
-    async trans(audioName) {
-        
-        const client2 = new SpeechClient();
-        // Creates a client
-        const audioBytes = audioName.data
-    
-        // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-        const audio = {
-          content: audioBytes
-        };
-        const config = {
-          encoding: 'OGG_OPUS',
-          sampleRateHertz: 16000,
-          languageCode: 'pt-BR',
-        };
-        const request = {
-          audio: audio,
-          config: config,
-        };
-    
-        // Detects speech in the audio file
-        const [response] = await client2.recognize(request);
-        const transcription = response.results
-          .map(result => result.alternatives[0].transcript)
-          .join('\n');
-    
-        return transcription
-        
-    }
+  async trans(audioName) {
+    const client2 = new SpeechClient();
+    // Creates a client
+    const audioBytes = audioName.data;
 
-    async viraAudio(frase, message){
-      return new Promise((resolve, reject) => {
-        const gtts = new gTTS(frase, 'pt-br');
-        const audioPath = './voz.mp3';
-    
-        gtts.save(audioPath, function (err, result) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(audioPath);
-          }
-        });
+    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+    const audio = {
+      content: audioBytes,
+    };
+    const config = {
+      encoding: "OGG_OPUS",
+      sampleRateHertz: 16000,
+      languageCode: "pt-BR",
+    };
+    const request = {
+      audio: audio,
+      config: config,
+    };
+
+    // Detects speech in the audio file
+    const [response] = await client2.recognize(request);
+    const transcription = response.results
+      .map((result) => result.alternatives[0].transcript)
+      .join("\n");
+
+    return transcription;
+  }
+
+  async viraAudio(frase, message) {
+    return new Promise((resolve, reject) => {
+      const gtts = new gTTS(frase, "pt-br");
+      const audioPath = "./voz.mp3";
+
+      gtts.save(audioPath, function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(audioPath);
+        }
       });
-      
-    }
- 
+    });
+  }
 }
 
-export default new Funcoes()
+export default new Funcoes();
